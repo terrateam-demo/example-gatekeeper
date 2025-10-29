ed591a2f390e9df80c3b1f4c81c6bea6fd9b640a
joshpollara <75546+joshpollara@users.noreply.github.com>
2025-10-29 10:57:48 +0100

    Refactor main.tf to remove unused resources
    Removed old terraform configuration and null resources.

--- production/compute/main.tf
+++ production/compute/main.tf
@@ -1,30 +1,4 @@
-terraform {
-  backend "gcs" {
-    bucket = "terrateam-demo-state"
-    prefix = "/tarmac/production/compute/tf.state"
-  }
-}
-
-module "staging_compute_instance" {
-  source = "../../modules/compute"
-
-  instance_count = 0
-  instance_name  = "production-instance"
-  instance_zone  = "us-central1-a"
-  boot_disk_image = "debian-cloud/debian-11"
-  network_name    = "default"
-  project_id      = "terrateam-sandbox"
-  region          = "us-central1"
-  provider_zone   = "us-central1-c"
-}
-
-resource "null_resource" "foobar" {
-  provisioner "local-exec" {
-    command = "echo change is to force a terrateam run in this dir"
-  }
-}
-
-resource "null_resource" "foobar1" {
+resource "null_resource" "foo" {
   provisioner "local-exec" {
     command = "echo this is a null resource"
   }
